b415cde69899909d3f92fc97a5eaa66f1d8c1b16
audreyteles <audreytelesdossantos@gmail.com>
2024-11-03 02:02:41 -0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+#[macro_use]
+extern crate rocket;
+
+use rocket::form::Form;
+use rocket::fs::TempFile;
+
+#[derive(FromForm)]
+struct Upload<'r> {
+    file: TempFile<'r>,
+}
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hi, frust!"
+}
+
+#[post("/upload", data = "<upload>")]
+async fn upload(mut upload: Form<Upload<'_>>) -> String {
+    // get file name
+    let file_name = upload.file.name().unwrap().to_string();
+
+    // Set path name
+    let path = format!("/home/audrey/Downloads/{}", file_name);
+
+    // Set the path reference with '&', because when we set this variable here, its moved
+    let result = upload.file.persist_to(&path).await;
+
+    // If there is an error when the file is persisted, generate an exception
+    if result.is_err() { panic!("{:?}", result.err().unwrap()); }
+
+    // Return success message
+    format!("File named {:?} has been saved!", file_name)
+}
+
+#[launch] // defines this function as main (without this function 'rocket' should be 'main')
+fn rocket() -> _ {
+    rocket::build()
+        .configure(rocket::Config::figment().merge(("port", 5000)))
+        .mount("/", routes![index])
+        .mount("/files", routes![upload])
+}
+
+
